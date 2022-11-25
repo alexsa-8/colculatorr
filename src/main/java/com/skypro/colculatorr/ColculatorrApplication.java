ce7a3acbfcc57c67d3571eb6def018ee06043b87
Alex <alexsa-8@mail.ru>
2022-11-25 21:29:04 +0300

    1

---
+++ src/main/java/com/skypro/colculatorr/ColculatorrApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.colculatorr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColculatorrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColculatorrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/colculatorr/ColculatorrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.colculatorr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColculatorrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
